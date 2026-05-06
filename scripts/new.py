dd5856d5b7d289d5efec19f45735ff5c606de465
DNT-Khoa <khoa.coder.jedi@gmail.com>
2026-05-06 18:16:38 +0200

    refactor: :zap: Use BufferedReader and PrintWrite as template when genering Solution base code for performance

--- scripts/new.py
+++ scripts/new.py
@@ -27,12 +27,19 @@ except ImportError:
 WORKSPACE = os.getcwd()
 
 SOLUTION_TEMPLATE = """\
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
         // your code here
+
+        pw.close();
     }
 }
 """

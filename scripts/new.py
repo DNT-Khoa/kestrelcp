d8ed2827b801ecc977e80dc31b6794ab135d39fd
DNT-Khoa <khoa.coder.jedi@gmail.com>
2026-05-16 22:13:00 +0200

    feat: :sparkles: Add javadoc on top of java solution so user can add Time, Space complexity, and useful notes for later reference

--- scripts/new.py
+++ scripts/new.py
@@ -31,6 +31,16 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+/**
+ * <b>Time complexity:</b> O(?)
+ *
+ * <p><b>Space complexity:</b> O(?)
+ *
+ * <p><b>Notes:</b>
+ * <ul>
+ *   <li></li>
+ * </ul>
+ */
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
